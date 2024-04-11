bfa5e73e2c2113ac2af78317a6e000e10eef1296
Ridwan-Lawal <lawal6382@gmail.com>
2024-04-11 11:43:51 +0100

    page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,20 @@
+import { PiArrowLeftBold } from "react-icons/pi";
+import { useNavigate } from "react-router-dom";
+
 function PageNotFound() {
-  return <div>page not found</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="h-screen p-8">
+      <button onClick={() => navigate(-1)}>
+        <PiArrowLeftBold className="text-[26px] text-gray-800 font-bold" />
+      </button>
+
+      <div className="flex flex-col justify-center items-center h-[70vh] gap-2">
+        <p className="text-2xl">👻</p>
+        <p className="text-blue-600 text-2xl font-medium">Page not found!</p>
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
